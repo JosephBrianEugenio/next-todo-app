116607f094a9658191ba9a165a7c1231295843f0
Joseph Brian Eugenio <jbeugenio34@gmail.com>
2024-06-25 19:25:45 +0800

    added

--- src/app/components/home/BoardCardComponent.js
+++ src/app/components/home/BoardCardComponent.js
@@ -3,20 +3,20 @@ import { useState } from "react";
 import TaskFormCard from "./TaskForm";
 
 const BoardCardComponent = () => {
-  const [hasTask, setHasTask] = useState(false);
+  const [hasTask, setHasTask] = useState(true);
 
   const statusOptions = ["Todo", "Pending", "Done"];
 
   return (
     <div className="flex gap-3 p-4">
-      <div className="flex-none w-64 h-full bg-white p-4 rounded-lg shadow-xl border-2">
+      <div className="flex-none w-64 h-full bg-white p-4 rounded-lg shadow-xl border-2 card">
         <div className="mb-2 text-lg font-medium text-gray-900 border-b-2 border-gray-200">
           Card Title
         </div>
 
         {/* Draggable card containers */}
         {hasTask ? (
-          <div className="text-sm text-gray-700 bg-gray-200 p-4 rounded-lg shadow-md border-2 max-w-sm overflow-hidden mb-2">
+          <div className="draggable-card text-sm text-gray-700 bg-gray-200 p-4 rounded-lg shadow-md border-2 max-w-sm overflow-hidden mb-2">
             <div className="py-4">
               <div className="mb-2 text-lg font-medium text-gray-900">
                 The Coldest Sunset
@@ -32,7 +32,6 @@ const BoardCardComponent = () => {
           </div>
         ) : (
           <div className="text-sm text-gray-700 bg-gray-200 py-2 px-1 rounded-lg shadow-md border-2 max-w-sm overflow-hidden">
-            
           <TaskFormCard />
           </div>
         )}
--- src/app/components/home/TaskForm.js
+++ src/app/components/home/TaskForm.js
@@ -1,48 +1,78 @@
+import { useState } from "react";
+
 const TaskFormCard = () => {
-    const statusOptions = ["Todo", "Pending", "Done"];
-    return ( 
-        <>
-          <form className="py-4">
-              <p className="text-sm text-gray-700 mb-2">Create a task</p>
-              <input
-                type="text"
-                id="taskName"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2"
-                placeholder="Task name"
-              />
-              <textarea
-                id="taskDesc"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2"
-                placeholder="Task Description"
-              />
-              <input
-                type="date"
-                id="taskStatus"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2"
-                placeholder="Status"
-              />
-              <select
-                id="status"
-                name="status"
-                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm mb-4"
-              >
-                {statusOptions.map((option) => (
-                  <option key={option} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
-              <div className="flex justify-end">
-                <button
-                  type="submit"
-                  className="bg-indigo-500 hover:bg-indigo-600 text-white p-2 rounded-lg"
-                >
-                  Add task
-                </button>
-              </div>
-            </form>
-        </>
-     );
-}
- 
+  const [showForm, setShowForm] = useState(false);
+  const statusOptions = ["Todo", "Pending", "Done"];
+
+  const handleAddTaskClick = () => {
+    setShowForm(true);
+  };
+  const handleCancelTaskClick = () => {
+    setShowForm(false);
+  };
+  const onHandleSubmitForm = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      {!showForm ? (
+        <div className="text-center">
+          <button
+            onClick={handleAddTaskClick}
+            className="btn bg-indigo-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Add task
+          </button>
+        </div>
+      ) : (
+        <form className="py-4 px-1" onSubmit={onHandleSubmitForm}>
+          <p className="text-sm text-gray-700 mb-2">Create a task</p>
+          <input
+            type="text"
+            id="taskName"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-2"
+            placeholder="Task name"
+          />
+          <textarea
+            id="taskDesc"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-2"
+            placeholder="Task Description"
+          />
+          <input
+            type="date"
+            id="taskDueDate"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-2"
+          />
+          <select
+            id="status"
+            name="status"
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm mb-4"
+          >
+            {statusOptions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="bg-indigo-500 hover:bg-indigo-600 text-white py-2 px-4 mr-2 rounded-lg"
+            >
+              Add
+            </button>
+            <button
+              type="button"
+              className="pt-1"
+              onClick={handleCancelTaskClick}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      )}
+    </>
+  );
+};
+
 export default TaskFormCard;
